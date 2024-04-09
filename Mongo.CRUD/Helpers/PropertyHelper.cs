27de29f6c8f1c905ecf94ebb3f509c85c0199003
Guilherme Barros | Stone <guilherme.sbarros@stone.com.br>
2024-04-09 20:01:08 -0300

    chore: reduce asymptotic time caching reflection info

--- Mongo.CRUD/Helpers/PropertyHelper.cs
+++ Mongo.CRUD/Helpers/PropertyHelper.cs
@@ -1,6 +1,8 @@
 ﻿using Mongo.CRUD.Models;
 using System;
+using System.Collections.Concurrent;
 using System.Linq;
+using System.Reflection;
 
 namespace Mongo.CRUD.Helpers
 {
@@ -9,6 +11,12 @@ namespace Mongo.CRUD.Helpers
     /// </summary>
     public static class PropertyHelper
     {
+        private static readonly ConcurrentDictionary<(Type Type, string PropertyName), PropertyInfo> PropertyInfoByNameCache
+            = new ConcurrentDictionary<(Type, string), PropertyInfo>();
+
+        private static readonly ConcurrentDictionary<(Type Type, Type AttributeType), (PropertyInfo PropertyInfo, Attribute[] Attributes)> PropertyInfoByAttributeCache
+            = new ConcurrentDictionary<(Type, Type), (PropertyInfo, Attribute[])>();
+
         /// <summary>
         /// Get single property details by property name
         /// </summary>
@@ -29,19 +37,27 @@ namespace Mongo.CRUD.Helpers
                 throw new ArgumentNullException(nameof(propertyName));
             }
 
-            foreach (var propertyInfo in obj.GetType().GetProperties())
+            var propertyInfo = PropertyInfoByNameCache.GetOrAdd((obj.GetType(), propertyName.Trim()), (key) =>
             {
-                if (propertyInfo.Name.ToLowerInvariant() == propertyName.Trim().ToLowerInvariant())
+                var properties = key.Type.GetProperties();
+                for (int i = 0; i < properties.Length; i++)
                 {
-                    var value = propertyInfo.GetValue(obj);
-                    return new PropertyDetails
+                    if (properties[i].Name.Equals(key.PropertyName, StringComparison.OrdinalIgnoreCase))
                     {
-                        PropertyInfo = propertyInfo,
-                        Value = value
-                    };
+                        return properties[i];
+                    }
                 }
+                return null;
+            });
+            if (propertyInfo != null)
+            {
+                var value = propertyInfo.GetValue(obj);
+                return new PropertyDetails
+                {
+                    PropertyInfo = propertyInfo,
+                    Value = value
+                };
             }
-
             return null;
         }
 
@@ -61,20 +77,28 @@ namespace Mongo.CRUD.Helpers
                 throw new ArgumentNullException(nameof(obj));
             }
 
-            foreach (var propertyInfo in obj.GetType().GetProperties())
+            var (propertyInfo, attributes) = PropertyInfoByAttributeCache.GetOrAdd((obj.GetType(), typeof(TAttribute)), (key) =>
             {
-                TAttribute[] attributes = (TAttribute[])propertyInfo.GetCustomAttributes(typeof(TAttribute), true);
-
-                if (attributes != null & attributes.Any())
+                var properties = key.Type.GetProperties();
+                for (int i = 0; i < properties.Length; i++)
                 {
-                    var value = propertyInfo.GetValue(obj);
-                    return new PropertyDetails<TAttribute>
+                    var attrs = (Attribute[])properties[i].GetCustomAttributes(key.AttributeType, true);
+                    if (attrs != null & attrs.Any())
                     {
-                        Attributes = attributes,
-                        PropertyInfo = propertyInfo,
-                        Value = value
-                    };
+                        return (properties[i], attrs);
+                    }
                 }
+                return (null, null);
+            });
+            if (propertyInfo != null && attributes != null) 
+            {
+                var value = propertyInfo.GetValue(obj);
+                return new PropertyDetails<TAttribute>
+                {
+                    Attributes = (TAttribute[])attributes,
+                    PropertyInfo = propertyInfo,
+                    Value = value
+                };
             }
 
             return null;
